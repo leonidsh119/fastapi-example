1d3594693b1935cbcf0cfd64c52d3276d8aa4e28
Leon <28947655+leonidsh119@users.noreply.github.com>
2025-01-11 18:26:36 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY . .
 
 RUN poetry install --no-root
 
+ENV APP_HOST="0.0.0.0"
+
 EXPOSE 8000
 
 ENTRYPOINT ["poetry", "run", "python", "-m", "main"]
--- app/rabbitmq/listener.py
+++ app/rabbitmq/listener.py
@@ -1,3 +1,5 @@
+from xmlrpc.client import boolean
+from aio_pika.abc import AbstractRobustConnection
 import aio_pika
 import asyncio
 from app.core.settings import settings
@@ -12,18 +14,35 @@ async def on_message(message: aio_pika.abc.AbstractIncomingMessage):
         message_payload: str = message.body.decode()
         logger.info(f"Received message: {message_payload}")
 
+# async def start_rabbitmq_listener():
+#
+#     logger.info(f"Connecting to RabbitMQ: {RABBITMQ_URL} ...")
+#     connection = await aio_pika.connect_robust(RABBITMQ_URL)
+#     logger.info(f"Connected to RabbitMQ: {RABBITMQ_URL}")
+#     async with connection:
+#         channel = await connection.channel()
+#         queue = await channel.declare_queue(settings.RABBITMQ_SUBSCRIBER_QUEUE, durable=True)
+#         logger.info(f"Waiting for messages in the queue {settings.RABBITMQ_SUBSCRIBER_QUEUE} ...")
+#         await queue.consume(on_message)
+#         await asyncio.Future()
+
 async def start_rabbitmq_listener():
     while True:
         try:
+            logger.info(f"Connecting to RabbitMQ: {RABBITMQ_URL} ...")
             connection = await aio_pika.connect_robust(RABBITMQ_URL)
             logger.info(f"Connected to RabbitMQ: {RABBITMQ_URL}")
             async with connection:
                 channel = await connection.channel()
                 queue = await channel.declare_queue(settings.RABBITMQ_SUBSCRIBER_QUEUE, durable=True)
-                logger.info("Waiting for messages in the queue {settings.RABBITMQ_SUBSCRIBER_QUEUE} ...")
+                logger.info(f"Waiting for messages in the queue {settings.RABBITMQ_SUBSCRIBER_QUEUE} ...")
                 await queue.consume(on_message)
-
+                await asyncio.Future()
         except aio_pika.exceptions.AMQPConnectionError as e:
             logger.error(f"Connection failed: {e}. Retrying in 5 seconds...")
-            await asyncio.sleep(5)  # Retry interval
+            await asyncio.sleep(5)
+
+
+
+
 
