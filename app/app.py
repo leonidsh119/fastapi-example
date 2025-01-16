ea7b61edaa57f6171e4fc35328a458f7d1a9c664
Leon <28947655+leonidsh119@users.noreply.github.com>
2025-01-16 14:55:16 +0200

    using lifespan events on fastapi

--- app/app.py
+++ app/app.py
@@ -2,14 +2,32 @@ import asyncio
 import logging
 from logging import config
 from fastapi import FastAPI
+from pathlib import Path
+from contextlib import asynccontextmanager
 from app.routers import health
 from app.routers import items
 from app.routers import notification
 from app.rabbitmq.listener import start_rabbitmq_listener
 from app.core.settings import settings
-from pathlib import Path
+
 
 config.fileConfig(fname=Path(__file__).resolve().parent.parent / "logging.conf")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup event - runs when the app starts
+    asyncio.create_task(start_rabbitmq_listener())
+    logging.info(f"FastAPI application started. Listening on RabbitMQ at {settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}.")
+
+    # Yield control to FastAPI to continue running the app
+    yield
+
+    # Shutdown event - runs when the app shuts down
+    logging.info("FastAPI application is shutting down.")
+    # Optionally, perform any necessary cleanup, such as stopping tasks, closing connections, etc.
+    # await stop_rabbitmq_listener()
+
 
 app = FastAPI(
     title="My FastAPI Project",
@@ -17,18 +35,10 @@ app = FastAPI(
     version="1.0.0",
     docs_url="/docs",
     redoc_url="/redoc",
+    lifespan=lifespan
 )
+
 
 app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(items.router, prefix="/items", tags=["items"])
 app.include_router(notification.router, prefix="/notification", tags=["notification"])
-
-@app.on_event("startup")
-async def startup_event():
-    # Start the RabbitMQ listener in the background
-    asyncio.create_task(start_rabbitmq_listener())
-    logging.info(f"FastAPI application started. Listening on RabbitMQ at {settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}.")
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    logging.info("FastAPI application shutting down...")
